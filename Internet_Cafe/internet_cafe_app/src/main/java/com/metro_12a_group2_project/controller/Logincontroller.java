e256399091c551473a06071fa27c2a5a9571baf6
Tobi <ryoko12fu@gmail.com>
2025-06-10 14:00:12 +0630

    remove random file

--- Internet_Cafe/internet_cafe_app/src/main/java/com/metro_12a_group2_project/controller/Logincontroller.java
+++
@@ -1,5 +0,0 @@
-package com.metro_12a_group2_project.controller;
-
-public class Logincontroller {
-    
-}
--- Internet_Cafe/internet_cafe_app/src/main/java/com/metro_12a_group2_project/controller/PrimaryController.java
+++ Internet_Cafe/internet_cafe_app/src/main/java/com/metro_12a_group2_project/controller/PrimaryController.java
@@ -14,7 +14,7 @@ public class PrimaryController {
 
     @FXML
     public void initialize(){
-
+        
     }
 
     @FXML
